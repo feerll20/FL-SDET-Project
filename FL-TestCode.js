873d110a355ed3b41d06f5eab969d0db20242a17
Fernando Lopez <fernando.lopez@ALLI-MC-180613B.local>
2019-11-13 23:28:08 -0600

    Github task from SDET course 1sqr

---
+++ FL-TestCode.js
@@ -0,0 +1,5 @@
+const myName = 'Fernando Lopez';
+const courseName = 'SDET'
+const companyName = 'Unosquare'
+
+console.log('Hello my name is '+ myName + ' and I am taking the ' + courseName + ' at ' + companyName);
