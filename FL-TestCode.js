1274cd708e7d1cc8353d3f7ea2aeaeb03240fbf2
Fernando Lopez <fernando.lopez@ALLI-MC-180613B.local>
2019-11-21 16:53:44 -0600

    uploading the file for exercise

---
+++ FL-TestCode.js
@@ -0,0 +1,5 @@
+const myName = 'Fernando Lopez';
+const courseName = 'SDET'
+const companyName = 'Unosquare'
+
+console.log('Hello my name is '+ myName + ' and this task is for the ' + courseName + ' coourse from ' + companyName);
